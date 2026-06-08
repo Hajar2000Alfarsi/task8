ec3340dbae8e3496082074bd30e35b1ab36f6a1e
Hajar <hajar2000alfarsi@gmail.com>
2026-06-08 11:10:05 +0400

    Implment constructor, getters, setters, and toString

--- src/Employee.java
+++ src/Employee.java
@@ -3,5 +3,41 @@ public class Employee {
     private String employeeName;
     private String department;
 
+    public Employee(String employeeId, String employeeName, String department) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.department = department;
+    }
 
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee: \n" +
+                "employeeId: " + employeeId + '\n' +
+                "employeeName: " + employeeName + '\n' +
+                "department: " + department ;
+    }
 }
