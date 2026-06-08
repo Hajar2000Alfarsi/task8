5bc4426329e2a6c262c68172a9e109ceda51b219
Hajar <hajar2000alfarsi@gmail.com>
2026-06-08 11:24:43 +0400

    Check validation

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,27 @@ public class Main {
         //New Employee data
         Employee newEmployee = new Employee("E104", "Ali", "IT");
 
+        //Validation - Check Duplicate ID
+        boolean exists = false;
 
+        for (Employee employee : employees) {
+            if (employee.getEmployeeId().equals(newEmployee.getEmployeeId())){
+                exists = true;
+                break;
+            }
+        }
+
+        if (exists) {
+            System.out.println("\nEmployee ID already exists.");
+            System.out.println("No employee was created.");
+        } else {
+            employees.add(newEmployee);
+
+            System.out.println("\nEmployee Added Successfully");
+            System.out.println("Employee ID: " + newEmployee.getEmployeeId());
+            System.out.println("Employee Name: " + newEmployee.getEmployeeName());
+            System.out.println("Department: " + newEmployee.getDepartment());
+            System.out.println("Status: Created");
+        }
     }
 }
