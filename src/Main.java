8d9226834065f0eeecfb428925d37aef5ee078f7
Hajar <hajar2000alfarsi@gmail.com>
2026-06-08 11:12:06 +0400

    Add 3 employees

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        //Add three employee
+
+        employees.add(new Employee("E101", "Sara", "HR"));
+        employees.add(new Employee("E102", "Ahmed", "Finance"));
+        employees.add(new Employee("E103", "John", "Sales"));
 
     }
 }
